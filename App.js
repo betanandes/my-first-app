33e4467043ec20743d73789d2b6ed9c851839c5b
betanandes <robertafernandes46@gmail.com>
2026-01-12 18:50:01 -0300

    feat: teste

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet} from "react-native";
+import Login from "./src/login";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+  return <Login />;
 }
 
-const styles = StyleSheet.create({
+export default App;
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: { 
+    fontSize: 40,
+    fontWeight: 'bold',
+    position: 'center',
+    color: 'blue'
   },
 });
---
+++ src/login.js
@@ -0,0 +1,60 @@
+import { View, StyleSheet, TextInput, TouchableOpacity, Text, Alert} from "react-native";
+import {useState} from "react";
+
+const Login = () => {
+    const [login, setLogin] = useState();
+    const [senha, setSenha] = useState();
+
+    function clickBtn() {
+        Alert.alert("login", login);
+        Alert.alert("senha", senha);
+    }
+
+    return <View style = {style.container}>
+        <Text style = {style.titulo}>Olá, bem-vindo!</Text>
+        <TextInput onChangeText={setLogin} placeholder="Login" style = {style.input} />
+        <TextInput keyboardType="numeric" onChangeText={setSenha} placeholder="Senha" style = {style.input} secureTextEntry={true} />
+        <TouchableOpacity onPress={clickBtn} style = {style.btn}>
+            <Text style = {style.textbtn}>Entrar</Text>
+        </TouchableOpacity>
+    </View>;
+};
+
+export default Login;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: { 
+    width: "80%",
+    backgroundColor: '#D3DEF4',
+    padding: 5,
+    fontSize: 16,
+    borderRadius: 6,
+    marginBottom: 10,
+  },
+  btn: {
+    width: "80%",
+    backgroundColor: '#4C6EF5',
+    padding: 10,
+    fontSize: 16,
+    borderRadius: 6,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  textbtn: { 
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  titulo: { 
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    position: 'center',
+    color: 'blue'
+  },
+});
